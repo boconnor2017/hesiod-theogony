539d221479a86caffc2dfabc53cfa24fb76d896b
Brendan <brendan.oconnor@broadcom.com>
2026-06-02 12:02:23 -0400

    refresh v1

--- ubuntu/refresh-ubuntu.sh
+++ ubuntu/refresh-ubuntu.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
+# To download this script run from root: sudo curl https://raw.githubusercontent.com/boconnor2017/hesiod-theogony/refs/heads/main/ubuntu/refresh-ubuntu.sh >> refresh-ubuntu.sh
 set -e
+rm -r /usr/local/hesiod-theogony
 git clone https://github.com/boconnor2017/hesiod-theogony.git /usr/local/hesiod-theogony
